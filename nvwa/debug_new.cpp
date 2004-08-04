7e4bc7c549989b5cbb6b2c41df91fda3784ad0f0
Wu Yongwei <wuyongwei@gmail.com>
2004-08-04 15:31:23 +0000

    Add code to detect the wrong usage of new[]/delete or new/delete[].

--- nvwa/debug_new.cpp
+++ nvwa/debug_new.cpp
@@ -31,12 +31,14 @@
  *
  * Implementation of debug versions of new and delete to check leakage.
  *
- * @version 2.7, 2004/08/04
+ * @version 2.8, 2004/08/04
  * @author  Wu Yongwei
  *
  */
 
 #include <new>
+#include <assert.h>
+#include <limits.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "fast_mutex.h"
@@ -148,6 +150,73 @@ bool new_autocheck_flag = true;
 FILE* new_output_fp = stderr;
 
 /**
+ * Searches for the raw pointer given a user pointer.  The term `raw
+ * pointer' here refers to the pointer to the pointer to originally
+ * <code>malloc</code>'d memory.
+ *
+ * @param pointer       user pointer to search for
+ * @param hash_index    hash index of the user pointer
+ * @return              the raw pointer if searching is successful; or
+ *                      \c NULL otherwise
+ */
+static new_ptr_list_t** search_pointer(void* pointer, size_t hash_index)
+{
+    new_ptr_list_t** raw_ptr = &new_ptr_list[hash_index];
+    while (*raw_ptr)
+    {
+        if ((char*)*raw_ptr + sizeof(new_ptr_list_t) == pointer)
+        {
+            return raw_ptr;
+        }
+        raw_ptr = &(*raw_ptr)->next;
+    }
+    return NULL;
+}
+
+/**
+ * Frees memory and adjusts pointers relating to a raw pointer.  If the
+ * highest bit of \c line (set from a previous <code>new[]</code> call)
+ * does not agree with \c array_mode, program will abort with an error
+ * message.
+ *
+ * @param raw_ptr       raw pointer to free
+ * @param array_mode    flag indicating whether it is invoked by a
+ *                      <code>delete[]</code> call
+ */
+static void free_pointer(new_ptr_list_t** raw_ptr, bool array_mode)
+{
+    new_ptr_list_t* ptr = *raw_ptr;
+    int array_mode_mismatch = array_mode ^ ((ptr->line & INT_MIN) != 0);
+    if (array_mode_mismatch)
+    {
+        const char* msg;
+        if (array_mode)
+            msg = "delete[] after new";
+        else
+            msg = "delete after new[]";
+        fprintf(new_output_fp,
+                "%s at %p (size %u, allocated from %s:%d)\n",
+                msg,
+                (char*)ptr + sizeof(new_ptr_list_t),
+                ptr->size,
+                ptr->file,
+                ptr->line & ~INT_MIN);
+        abort();
+    }
+    total_mem_alloc -= ptr->size;
+    if (new_verbose_flag)
+    {
+        fprintf(new_output_fp,
+                "delete: freeing  %p (size %u, %u bytes still allocated)\n",
+                (char*)ptr + sizeof(new_ptr_list_t),
+                ptr->size, total_mem_alloc);
+    }
+    *raw_ptr = ptr->next;
+    free(ptr);
+    return;
+}
+
+/**
  * Checks for memory leaks.
  *
  * @return  zero if no leakage is found; the number of leaks otherwise
@@ -168,7 +237,7 @@ int check_leaks()
                     (char*)ptr + sizeof(new_ptr_list_t),
                     ptr->size,
                     ptr->file,
-                    ptr->line);
+                    ptr->line & ~INT_MIN);
             ptr = ptr->next;
             ++leak_cnt;
         }
@@ -178,6 +247,7 @@ int check_leaks()
 
 void* operator new(size_t size, const char* file, int line)
 {
+    assert((line & INT_MIN) == 0);
     size_t s = size + sizeof(new_ptr_list_t);
     new_ptr_list_t* ptr = (new_ptr_list_t*)malloc(s);
     if (ptr == NULL)
@@ -213,7 +283,12 @@ void* operator new(size_t size, const char* file, int line)
 
 void* operator new[](size_t size, const char* file, int line)
 {
-    return operator new(size, file, line);
+    void* pointer = operator new(size, file, line);
+    new_ptr_list_t* ptr =
+            (new_ptr_list_t*)((char*)pointer - sizeof(new_ptr_list_t));
+    assert((ptr->line & INT_MIN) == 0);
+    ptr->line |= INT_MIN;   // The highest bit indicates result from new[]
+    return pointer;
 }
 
 void* operator new(size_t size) throw(std::bad_alloc)
@@ -223,7 +298,7 @@ void* operator new(size_t size) throw(std::bad_alloc)
 
 void* operator new[](size_t size) throw(std::bad_alloc)
 {
-    return operator new(size);
+    return operator new[](size, "<Unknown>", 0);
 }
 
 void* operator new(size_t size, const std::nothrow_t&) throw()
@@ -242,37 +317,28 @@ void operator delete(void* pointer) throw()
         return;
     size_t hash_index = _DEBUG_NEW_HASH(pointer);
     fast_mutex_autolock lock(new_ptr_lock[hash_index]);
-    new_ptr_list_t* ptr = new_ptr_list[hash_index];
-    new_ptr_list_t* ptr_last = NULL;
-    while (ptr)
+    new_ptr_list_t** raw_ptr = search_pointer(pointer, hash_index);
+    if (raw_ptr == NULL)
     {
-        if ((char*)ptr + sizeof(new_ptr_list_t) == pointer)
-        {
-            total_mem_alloc -= ptr->size;
-            if (new_verbose_flag)
-            {
-                fprintf(new_output_fp,
-                        "delete: freeing  %p (size %u"
-                        ", %u bytes still allocated)\n",
-                        pointer, ptr->size, total_mem_alloc);
-            }
-            if (ptr_last == NULL)
-                new_ptr_list[hash_index] = ptr->next;
-            else
-                ptr_last->next = ptr->next;
-            free(ptr);
-            return;
-        }
-        ptr_last = ptr;
-        ptr = ptr->next;
+        fprintf(new_output_fp, "delete: invalid pointer %p\n", pointer);
+        abort();
     }
-    fprintf(new_output_fp, "delete: invalid pointer %p\n", pointer);
-    abort();
+    free_pointer(raw_ptr, false);
 }
 
 void operator delete[](void* pointer) throw()
 {
-    operator delete(pointer);
+    if (pointer == NULL)
+        return;
+    size_t hash_index = _DEBUG_NEW_HASH(pointer);
+    fast_mutex_autolock lock(new_ptr_lock[hash_index]);
+    new_ptr_list_t** raw_ptr = search_pointer(pointer, hash_index);
+    if (raw_ptr == NULL)
+    {
+        fprintf(new_output_fp, "delete[]: invalid pointer %p\n", pointer);
+        abort();
+    }
+    free_pointer(raw_ptr, true);
 }
 
 // Some older compilers like Borland C++ Compiler 5.5.1 and Digital Mars
@@ -296,7 +362,14 @@ void operator delete(void* pointer, const char* file, int line) throw()
 
 void operator delete[](void* pointer, const char* file, int line) throw()
 {
-    operator delete(pointer, file, line);
+    if (new_verbose_flag)
+    {
+        fprintf(new_output_fp,
+                "info: exception thrown on initializing objects"
+                " at %p (%s:%d)\n",
+                pointer, file, line);
+    }
+    operator delete[](pointer);
 }
 
 void operator delete(void* pointer, const std::nothrow_t&) throw()
@@ -306,14 +379,14 @@ void operator delete(void* pointer, const std::nothrow_t&) throw()
 
 void operator delete[](void* pointer, const std::nothrow_t&) throw()
 {
-    operator delete(pointer, std::nothrow);
+    operator delete[](pointer, "<Unknown>", 0);
 }
 #endif // NO_PLACEMENT_DELETE
 
 int __debug_new_counter::_count = 0;
 
 /**
- *  Constructor to increment the count.
+ * Constructor to increment the count.
  */
 __debug_new_counter::__debug_new_counter()
 {
