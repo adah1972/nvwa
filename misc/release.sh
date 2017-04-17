a4263ff41834664d962a8eb0329a24b3a0467c4d
Wu Yongwei <wuyongwei@gmail.com>
2017-04-17 12:34:13 +0000

    Use one shell script to generate both the Unix and Windows packages; Increase version to 1.1.

--- misc/release.sh
+++ misc/release.sh
@@ -3,17 +3,31 @@
 
 # Shell script to create "nvwa-x.y.tar.gz"
 
-VER="1.0"
+VER="1.1"
 RELEASE="nvwa-$VER"
+RELEASE_DIR="$PWD/$RELEASE"
+rm -f $RELEASE.tar.gz $RELEASE.zip
 rm -rf $RELEASE
 mkdir $RELEASE
-cd $RELEASE
-
-export CVSROOT=:pserver:anonymous@nvwa.cvs.sourceforge.net:/cvsroot/nvwa
-cvs -z9 co doc nvwa
-
-cp -pr ../../doc/latex/refman.pdf ../../doc/html doc/
-( cd doc/html; rm -f *.dot *.map *.md5 .* )
 
 cd ..
+cp -p ChangeLog LICENCE README $RELEASE_DIR
+
+mkdir "$RELEASE_DIR/nvwa"
+( cd "nvwa"; cp -p *.cpp *.h "$RELEASE_DIR/nvwa" )
+
+mkdir "$RELEASE_DIR/test"
+( cd "test"; cp -p Makefile *.cpp "$RELEASE_DIR/test" )
+
+mkdir "$RELEASE_DIR/doc"
+cp -pr doc/html "$RELEASE_DIR/doc/html"
+( cd "$RELEASE_DIR/doc/html"; rm -f *.map *.md5 )
+
+cd misc
 tar cvfz $RELEASE.tar.gz $RELEASE/
+
+FILES=`find $RELEASE -type f`
+for FILE in $FILES; do
+  unix2dos -k -s $FILE
+done
+zip -r $RELEASE.zip $RELEASE/
