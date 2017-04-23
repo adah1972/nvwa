cfbc88577059892b51400d70031377dd22d2fb72
Wu Yongwei <wuyongwei@gmail.com>
2017-04-23 16:26:55 +0800

    Replace `sedfile' with `sed -i'.

--- misc/doxit.sh
+++ misc/doxit.sh
@@ -86,22 +86,22 @@ fi
 # Set the options in the intermediate Doxyfile
 cp -p Doxyfile $DOXYFILE_TMP
 if [ "$GENERATE_LATEX" = "YES" ]; then
-  sedfile 's/\(GENERATE_LATEX *=\).*/\1 YES/' $DOXYFILE_TMP
+  sed -i '' 's/\(GENERATE_LATEX *=\).*/\1 YES/' $DOXYFILE_TMP
   if [ "$PDF_HYPERLINKS" = "YES" ]; then
-    sedfile 's/\(PDF_HYPERLINKS *=\).*/\1 YES/' $DOXYFILE_TMP
+    sed -i '' 's/\(PDF_HYPERLINKS *=\).*/\1 YES/' $DOXYFILE_TMP
   else
-    sedfile 's/\(PDF_HYPERLINKS *=\).*/\1 NO/'  $DOXYFILE_TMP
+    sed -i '' 's/\(PDF_HYPERLINKS *=\).*/\1 NO/'  $DOXYFILE_TMP
   fi
   if [ "$USE_PDFLATEX" = "YES" ]; then
-    sedfile 's/\(USE_PDFLATEX *=\).*/\1 YES/' $DOXYFILE_TMP
+    sed -i '' 's/\(USE_PDFLATEX *=\).*/\1 YES/' $DOXYFILE_TMP
   else
-    sedfile 's/\(USE_PDFLATEX *=\).*/\1 NO/'  $DOXYFILE_TMP
+    sed -i '' 's/\(USE_PDFLATEX *=\).*/\1 NO/'  $DOXYFILE_TMP
   fi
 else
-  sedfile 's/\(GENERATE_LATEX *=\).*/\1 NO/'  $DOXYFILE_TMP
+  sed -i '' 's/\(GENERATE_LATEX *=\).*/\1 NO/'  $DOXYFILE_TMP
 fi
 if [ "$OLD_DOXYGEN_VER" = "YES" ]; then
-  sedfile 's/ \(.*nvwa\/functional\.h\)/#\1/' $DOXYFILE_TMP
+  sed -i '' 's/ \(.*nvwa\/functional\.h\)/#\1/' $DOXYFILE_TMP
 fi
 
 # Work around an expression that will confuse Doxygen
