c37a5a9ba8ae443d20ec25f6e0adbd4dbcd14195
Wu Yongwei <wuyongwei@gmail.com>
2020-01-20 13:45:52 +0800

    Use BOOST_TEST in split_test where possible.

--- test/boosttest_split.cpp
+++ test/boosttest_split.cpp
@@ -21,9 +21,9 @@ BOOST_AUTO_TEST_CASE(split_test)
     constexpr auto end = result.end();
     auto result_s = result.to_vector();
     auto result_sv = result.to_vector_sv();
-    BOOST_CHECK(result_s == split_result_expected);
-    BOOST_CHECK_EQUAL(result_s.size(), result_sv.size());
-    if (result_s.size() == result_sv.size()) {
+    BOOST_TEST(result_s == split_result_expected);
+    BOOST_TEST_REQUIRE(result_s.size() == result_sv.size());
+    {
         auto it = result_sv.begin();
         for (auto& s : result_s) {
             BOOST_CHECK_EQUAL(s, *it);
@@ -33,10 +33,9 @@ BOOST_AUTO_TEST_CASE(split_test)
     size_t i = 0;
     auto it = result.begin();
     for (; it != end && i < result_s.size(); ++it) {
-        BOOST_CHECK_EQUAL(*it, result_s[i]);
+        BOOST_TEST(*it ==result_s[i]);
         if (!result_s[i].empty()) {
-            BOOST_CHECK_EQUAL(nvwa::split(*it, "="s).to_vector_sv().size(),
-                              2U);
+            BOOST_TEST(nvwa::split(*it, "="s).to_vector_sv().size() == 2U);
         }
         ++i;
     }
