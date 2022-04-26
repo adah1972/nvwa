080428ba3cdee5aa6ea9a0737a56d7c65b0c12bc
Wu Yongwei <wuyongwei@gmail.com>
2022-04-26 23:46:27 +0800

    Check to ensure number_range satisfy input_range

--- test/number_range_test.cpp
+++ test/number_range_test.cpp
@@ -2,6 +2,11 @@
 #include <functional>
 #include <boost/test/unit_test.hpp>
 #include "nvwa/functional.h"
+#include "nvwa/c++_features.h"
+
+#if HAVE_CXX20_RANGES
+#include <ranges>
+#endif
 
 BOOST_AUTO_TEST_CASE(number_range_test)
 {
@@ -13,4 +18,8 @@ BOOST_AUTO_TEST_CASE(number_range_test)
     BOOST_CHECK_EQUAL(
         nvwa::reduce(std::plus<>(), nvwa::number_range(2.0, 200.0, 2.0)),
         9900.0);
+
+#if HAVE_CXX20_RANGES
+    static_assert(std::ranges::input_range<decltype(nvwa::number_range(1, 101))>);
+#endif
 }
